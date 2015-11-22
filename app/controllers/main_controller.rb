c6e9f4d09eb295ed996734b5d8a77c3ced8702fd
draconian00 <korea_syh@naver.com>
2015-11-22 02:18:35 +0000

    add_session

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
   
   def index
+    @objID = params[:objID]
     @main = Parse::Query.new("posts").value_in("tags", ["핫이슈"])
     @recent = Parse::Query.new("posts")
     @tag_arr = ["연예","스포츠","게임","시사"]
@@ -14,6 +15,7 @@ class MainController < ApplicationController
   end
   
   def post_list
+    
     @id = params[:id]
     @list = Parse::Query.new("posts").eq("main_tag", @id).or(Parse::Query.new("posts").value_in("tags", [@id]))
   end
@@ -23,6 +25,16 @@ class MainController < ApplicationController
     @post = Parse::Query.new("posts").eq("objectId", params[:id].to_s)
     
     @color_arr = ["#c2bfd9","#d0ecf2","#bad9bc","#edf2c9","#f2e2ce"]
+    
+    #통계치 부분 처리
+    @left_num = @post.get.first["left"]["value"]
+    @left_content = @post.get.first["left"]["content"]
+    @right_num = @post.get.first["right"]["value"]
+    @right_content = @post.get.first["right"]["content"]
+    @total = @left_num + @right_num
+    if @total == 0
+      @total = 1
+    end
     
     #댓글부분 처리
     repliesModel = Parse::Query.new("replies")
@@ -67,7 +79,58 @@ class MainController < ApplicationController
      redirect_to :back
   end
   
+  def do_vote
+      @original_id = params[:id]
+      post_id = /(.*)_(.*)/.match(params[:id])[1].to_s
+      select_index = /(.*)_(.*)/.match(params[:id])[2].to_i
+      
+      target_post = Parse::Query.new("posts").eq("objectId", post_id).get.first
+      
+      if select_index == 0
+        target_post["left"]["value"] += 1
+        target_post.save
+      elsif select_index == 1
+        target_post["right"]["value"] += 1
+        target_post.save
+      end
+      
+      redirect_to "/main/post_content/#{post_id}"
+  end
+  
   def user_page
   end
+  
+  def signuptest
+  end
+  
+  def after_signup
+    username = params[:username].to_s
+    password = params[:password].to_s
+    user = Parse::User.new({
+      :username => username,
+      :password => password
+    })
+    user.save
+    redirect_to "/main/logintest"
+  end
+  
+  def logintest
+  end
+  
+  #def after_login
+  #  username = params[:username]
+  #  password = params[:password]
+  #  
+  #  body = {
+  #    "username" => username,
+  #    "password" => password
+  #  }
+  #
+  #  @response = Parse.client.request(Parse::Protocol::USER_LOGIN_URI, :get, nil, body)
+  #  Parse.client.session_token = @response[Parse::Protocol::KEY_USER_SESSION_TOKEN]
+  #
+  #  #new(SUPER_class = response)
+  #  redirect_to '/'
+  #end
   
 end
